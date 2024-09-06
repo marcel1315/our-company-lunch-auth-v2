2e2557ebd25070d00854bbca78edc4d9377494de
Marcel Lee <chungchung1315@gmail.com>
2024-09-06 23:40:08 +0900

    chore: Init spring project

---
+++ src/main/java/hello/ourcompanylunchauthserver/OurCompanyLunchAuthServerApplication.java
@@ -0,0 +1,13 @@
+package hello.ourcompanylunchauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurCompanyLunchAuthServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OurCompanyLunchAuthServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/hello/ourcompanylunchauthserver/OurCompanyLunchAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.ourcompanylunchauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurCompanyLunchAuthServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
